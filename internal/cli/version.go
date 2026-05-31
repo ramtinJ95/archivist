7a1c9a358b58fcd5b78be5c4253a5d2c0600232a
ramtin <ramme95@gmail.com>
2026-05-31 20:49:28 +0200

    fix(cli): report tagged module versions

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,18 +2,40 @@ package cli
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 var Version = "dev"
 
+func currentVersion(moduleVersion string) string {
+	if Version != "" && Version != "dev" {
+		return Version
+	}
+	if moduleVersion != "" && moduleVersion != "(devel)" {
+		return moduleVersion
+	}
+	if Version == "" {
+		return "dev"
+	}
+	return Version
+}
+
+func buildInfoModuleVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	return info.Main.Version
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the archivist version",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Fprintln(cmd.OutOrStdout(), "archivist "+Version)
+		fmt.Fprintln(cmd.OutOrStdout(), "archivist "+currentVersion(buildInfoModuleVersion()))
 	},
 }
 
